16d895eec07b9e8eaee4b2780ca75adb821c81d6
Camilo Vargas-Cabrera <vargax@gmail.com>
2021-12-23 04:16:43 -0500

    Removing api app (to be merged with core)

--- djangoIPC/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from core.models import Country
-
-
-class CountrySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Country
-        fields = ['url', 'id', 'name', 'code']
--- djangoIPC/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('', api_root),
-    path('countries/', CountriesList.as_view(), name='countries-list'),
-    path('countries/<int:pk>/', CountyDetail.as_view(), name='country-detail'),
-]
